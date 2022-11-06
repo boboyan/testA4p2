f019dcd503a713ed15469280cf2b12875331ea4f
boboyan <byan@unomaha.edu>
2022-11-05 20:36:41 -0500

    1try

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\boboy\PycharmProjects\assignment4-boboyan\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
