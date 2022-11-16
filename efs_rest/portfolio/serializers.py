f64944f316b11bf71b056a32cdaeb6676216eca7
BoboYan <byan@unomaha.edu>
2022-11-16 09:32:45 -0600

    Update serializers.py

--- efs_rest/portfolio/serializers.py
+++ efs_rest/portfolio/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from rest_framework_jwt.serializers import User
-
 from .models import Customer, Investment, Stock
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
@@ -8,11 +6,9 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 
 
-
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-
 
         fields = ('pk', 'name', 'address', 'cust_number', 'city', 'state', 'zipcode', 'email', 'cell_phone')
 
@@ -21,13 +17,14 @@ class InvestmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investment
         fields = (
-        'pk', 'customer','cust_number', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value', 'recent_date')
+            'pk', 'customer', 'cust_number', 'category', 'description', 'acquired_value', 'acquired_date',
+            'recent_value', 'recent_date')
 
 
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
-        fields = ('pk', 'customer', 'cust_number','cust_number', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date')
+        fields = ('pk', 'customer', 'cust_number', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -37,7 +34,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'},
                                      validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'}, required=True)
 
     class Meta:
         model = User
@@ -46,7 +43,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             'first_name': {'required': True},
             'last_name': {'required': True},
             'password': {'write_only': True, 'min_length': 6},
-            'password2': {'read_only': True, 'min_length': 6}
+            'password2': {'write_only': True, 'min_length': 6}
         }
 
     def validate(self, attrs):
@@ -55,18 +52,14 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         return attrs
 
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
 
-def create(self, validated_data):
-    user = User.objects.create(
-        username=validated_data['username'],
-        email=validated_data['email'],
-        first_name=validated_data['first_name'],
-        last_name=validated_data['last_name']
-    )
-
-
-    user.set_password(validated_data['password'])
-    user.save()
-
-    return user
-
+        return user
